787e1dfc5526fa5c7312846ba97e19eeecddc1b3
Nebusoku Hibari <nebusokuhibari@gmail.com>
2018-09-22 10:40:00 +0900

    01 UniRx-HealthBar-Simple 完成

---
+++ 01 UniRx-HealthBar-Simple/Assets/Scripts/ParameterBarView.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParameterBarView : MonoBehaviour
+{
+
+    public RectTransform valueRect;
+
+    public void SetRate(float rate)
+    {
+        valueRect.localScale = new Vector3(rate, 1, 1);
+    }
+}
